ff689ddf12ffdef9fc89597139f194689ea4b526
sotaroiwasa <souwasao084@gmail.com>
2023-07-29 17:08:25 +0900

    add drawer-menu

--- hit-web/src/components/Header/Header.js
+++ hit-web/src/components/Header/Header.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css';
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(prevState => !prevState);
+    };
+    
     return (
         <header>
             <h1>Bashi.com</h1>
-            {/* その他に、ナビゲーションリンクなどをここに追加できます。 */}
-			<nav>
+            <div className="burger-menu" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            {/* ドロワーメニューのコード */}
+            {isMenuOpen && (
+                <div className="drawer-menu">
+                    <nav>
+                        <a href="#activity" onClick={toggleMenu}>Activity</a>
+                        <a href="#schedule" onClick={toggleMenu}>Schedule</a>
+                        <a href="#works" onClick={toggleMenu}>Works</a>
+                    </nav>
+                </div>
+            )}
+			
+            <nav className="header-menu">
                 <a href="#activity">Activity</a>
                 <a href="#schedule">Schedule</a>
                 <a href="#works">Works</a>
-            </nav>
+            </nav> 
         </header>
     );
 };
