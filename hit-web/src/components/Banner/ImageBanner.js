699adde766b1612e8d82e85aefa046212deccc71
sotaroiwasa <souwasao084@gmail.com>
2023-08-01 17:47:31 +0900

    modify imageBanner

--- hit-web/src/components/Banner/ImageBanner.js
+++ hit-web/src/components/Banner/ImageBanner.js
@@ -6,11 +6,13 @@ const ImageBanner = () => {
         <div className="image-banner">
             <img src="/images/hit.jpg" alt="説明文" />
 			<div className="banner-text">
-                <h2>Hello, World!</h2>
+                <h2>一橋大学 コンピュータ研究会</h2>
                 <a href="https://docs.google.com/forms/d/1XO1au5NUbYZrybL3rL5afMVSdvB_03nBRg1ZG3P1l1w/viewform?edit_requested=true"
                    target="_blank"
                    rel="noopener noreferrer"
-                   className="banner-button">入会はこちら</a>
+                   className="banner-button">
+                   入会はこちら
+                </a>
             </div>
         </div>
     );
