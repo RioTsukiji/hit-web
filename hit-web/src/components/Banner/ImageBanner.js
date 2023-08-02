10eb8f461761ec0a38ce19db98c68f05cf556cba
sotaroiwasa <souwasao084@gmail.com>
2023-08-03 04:28:16 +0900

    better image banner

--- hit-web/src/components/Banner/ImageBanner.js
+++ hit-web/src/components/Banner/ImageBanner.js
@@ -4,16 +4,16 @@ import './ImageBanner.css';
 const ImageBanner = () => {
     return (
         <div className="image-banner">
-            <img src="/images/hit.jpg" alt="説明文" />
+            <img src="/images/computer_club.png" alt="説明文" />
 			<div className="banner-text">
                 <h2>一橋大学 コンピュータ研究会</h2>
-                <a href="https://docs.google.com/forms/d/1XO1au5NUbYZrybL3rL5afMVSdvB_03nBRg1ZG3P1l1w/viewform?edit_requested=true"
-                   target="_blank"
-                   rel="noopener noreferrer"
-                   className="banner-button">
-                   入会はこちら
-                </a>
             </div>
+            <a href="https://docs.google.com/forms/d/1XO1au5NUbYZrybL3rL5afMVSdvB_03nBRg1ZG3P1l1w/viewform?edit_requested=true"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="banner-button">
+                入会はこちら
+            </a>
         </div>
     );
 };
